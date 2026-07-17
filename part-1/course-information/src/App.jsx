06e1338b9f826afb489c31d64b8fbfe5665eb60e
Mahmoud Abdelati <dr.mahmoud.hamed.abdelati@gmail.com>
2026-07-17 18:11:33 +0300

    added course-information application final version

---
+++ part-1/course-information/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} />
+      <Part part={props.part2} />
+      <Part part={props.part3} />
+    </div>
+  );
+};
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content
+        part1={course.parts[0]}
+        part2={course.parts[1]}
+        part3={course.parts[2]}
+      />
+      <Total
+        exercises1={course.parts[0].exercises}
+        exercises2={course.parts[1].exercises}
+        exercises3={course.parts[2].exercises}
+      />
+    </div>
+  );
+};
+
+export default App;
