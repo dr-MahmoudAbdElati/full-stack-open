28fe0e52459fa5cba1f3891c0949484fd7e962b9
Mahmoud Abdelati <dr.mahmoud.hamed.abdelati@gmail.com>
2026-08-04 11:52:31 +0300

    added a button to each blog to display or hide its details

--- part-5/bloglist-frontend/src/components/Blog.jsx
+++ part-5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const blogStyle = {
+    padding: 5,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return visible ? (
+    <div style={blogStyle}>
+      <p>
+        {blog.title} {"  "}
+        <button onClick={() => setVisible(false)}>hide</button>
+      </p>
+      <p>{blog.url}</p>
+      <p>
+        likes: {blog.likes} <button>like</button>
+      </p>
+      <p>{blog.author}</p>
+    </div>
+  ) : (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} {"  "}
+      <button onClick={() => setVisible(true)}>view</button>
+    </div>
+  );
+};
+
+export default Blog;
