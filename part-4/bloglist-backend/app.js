187ca33c235193a0bb6507c18782e8eb08a01c04
Mahmoud Abdelati <dr.mahmoud.hamed.abdelati@gmail.com>
2026-08-27 18:38:32 +0300

    initialized and configured e2e testing project for bloglist application using playwright library and added an endpoint to the backend to reset our test database before each test

--- part-4/bloglist-backend/app.js
+++ part-4/bloglist-backend/app.js
@@ -26,6 +26,11 @@ app.use("/api/login", loginRouter);
 app.use("/api/blogs", middleware.userExtractor, blogsRouter);
 app.use("/api/users", userRouter);
 
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
+
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
