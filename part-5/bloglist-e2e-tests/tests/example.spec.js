cd8ff2cc20c417eb4983f04d55e750c51d7d9057
Mahmoud Abdelati <dr.mahmoud.hamed.abdelati@gmail.com>
2026-08-27 19:03:53 +0300

    added e2e tests for both successful and failed login

--- part-5/bloglist-e2e-tests/tests/example.spec.js
+++ part-5/bloglist-e2e-tests/tests/example.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
+const { loginWith } = require("./helper");
 
 describe("Blog app", () => {
   beforeEach(async ({ page, request }) => {
@@ -18,5 +19,22 @@ describe("Blog app", () => {
     await expect(page.getByLabel("username")).toBeVisible();
     await expect(page.getByLabel("password")).toBeVisible();
     await expect(page.getByRole("button", { name: "login" })).toBeVisible();
+  });
+
+  describe("Login", () => {
+    test("succeeds with correct credentials", async ({ page }) => {
+      await loginWith(page, "mahmoud", "password");
+      await expect(page.getByText("mahmoud logged in")).toBeVisible();
+      await expect(page.getByRole("button", { name: "logout" })).toBeVisible();
+    });
+
+    test("fails with wrong credentials", async ({ page }) => {
+      await loginWith(page, "mahmoud", "wrong");
+      await expect(page.getByText("wrong username or password")).toBeVisible();
+      await expect(page.getByText("mahmoud logged in")).not.toBeVisible();
+      await expect(
+        page.getByRole("button", { name: "logout" }),
+      ).not.toBeVisible();
+    });
   });
 });
