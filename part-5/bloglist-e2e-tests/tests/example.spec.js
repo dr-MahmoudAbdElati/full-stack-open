d15ff6c4f11c70e2d90eb0e1e109805112a4d13d
Mahmoud Abdelati <dr.mahmoud.hamed.abdelati@gmail.com>
2026-09-05 08:09:38 +0300

    added playwright tests ensuring a blog can be deleted by the user who added it, only the user who added a blog can see the delete button and blogs are arranged in order of likes. blog with the most likes first

--- part-5/bloglist-e2e-tests/tests/example.spec.js
+++ part-5/bloglist-e2e-tests/tests/example.spec.js
@@ -77,5 +77,95 @@ describe("Blog app", () => {
       await blogToLike.getByRole("button", { name: "like" }).click();
       await expect(blogToLike).toContainText("likes: 1");
     });
+
+    test("a blog can be deleted by the user who added it", async ({ page }) => {
+      await createBlog(page, "good title", "mahmoud", "example url");
+      const blogToDelete = page
+        .locator("div")
+        .filter({ hasText: /^good/ })
+        .last();
+
+      await blogToDelete.getByRole("button", { name: "view" }).click();
+      page.on("dialog", (dialog) => dialog.accept());
+      await page.getByRole("button", { name: "remove" }).click();
+      await expect(blogToDelete).not.toBeVisible();
+    });
+
+    test("only the user who added a blog can see the delete button", async ({
+      page,
+      request,
+    }) => {
+      await createBlog(page, "good title", "mahmoud", "example url");
+      const blogToDelete = page
+        .locator("div")
+        .filter({ hasText: /^good/ })
+        .last();
+
+      await blogToDelete.getByRole("button", { name: "view" }).click();
+      await expect(
+        blogToDelete.getByRole("button", { name: "remove" }),
+      ).toBeVisible();
+
+      await page.getByRole("button", { name: "logout" }).click();
+      await request.post("http://localhost:3003/api/users", {
+        data: {
+          name: "another user",
+          username: "anotheruser",
+          password: "password",
+        },
+      });
+      await loginWith(page, "anotheruser", "password");
+
+      const blogToDeleteAfterLogout = page
+        .locator("div")
+        .filter({ hasText: /^good/ })
+        .last();
+
+      await blogToDeleteAfterLogout
+        .getByRole("button", { name: "view" })
+        .click();
+      await expect(
+        blogToDeleteAfterLogout.getByRole("button", { name: "remove" }),
+      ).not.toBeVisible();
+    });
+
+    test("blogs are arranged in order of likes. blog with the most likes first", async ({
+      page,
+    }) => {
+      await createBlog(page, "first blog", "mahmoud", "example url");
+      await createBlog(page, "second blog", "mahmoud", "example url");
+      await createBlog(page, "third blog", "mahmoud", "example url");
+
+      const firstBlog = page
+        .locator("div")
+        .filter({ hasText: /^first/ })
+        .last();
+      const secondBlog = page
+        .locator("div")
+        .filter({ hasText: /^second/ })
+        .last();
+      const thirdBlog = page
+        .locator("div")
+        .filter({ hasText: /^third/ })
+        .last();
+
+      await firstBlog.getByRole("button", { name: "view" }).click();
+      await secondBlog.getByRole("button", { name: "view" }).click();
+      await thirdBlog.getByRole("button", { name: "view" }).click();
+
+      await secondBlog.getByRole("button", { name: "like" }).click();
+      await secondBlog.getByRole("button", { name: "like" }).click();
+      await firstBlog.getByRole("button", { name: "like" }).click();
+
+      const blogs = page.locator("div").filter({ hasText: /blog/ });
+
+      const firstBlogText = await blogs.nth(0).textContent();
+      const secondBlogText = await blogs.nth(1).textContent();
+      const thirdBlogText = await blogs.nth(2).textContent();
+
+      expect(firstBlogText).toContain("second blog");
+      expect(secondBlogText).toContain("first blog");
+      expect(thirdBlogText).toContain("third blog");
+    });
   });
 });
